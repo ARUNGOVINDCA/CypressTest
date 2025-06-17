6ba2d916e4f8d9545478db92805d81ccb4e40ed9
arun <arun@ceroen.com>
2025-06-17 16:46:41 +0530

    Quote Added

--- cypress/e2e/Quotes.cy.js
+++ cypress/e2e/Quotes.cy.js
@@ -6,11 +6,20 @@ describe('Quote Creation', () => {
     });
     //  Creating New Quote
         it('should New Quote', () => {
-            cy.get('#pn_id_1_tab_1 > .pi').click();
+            cy.get('[routerlink="/quotes"]').click();
             cy.wait(1000);
-            cy.get(':nth-child(2) > .space-y-1 > li').click();
-            cy.wait(3000);
+            cy.get('.btn-new > .p-ripple').click();
+            cy.wait(2000);
+            cy.get('#clientDetails > .p-card > .p-card-body > .p-card-content > :nth-child(1) > :nth-child(1) > .p-floatlabel > #clientgroup > .p-select-label').click();
+            cy.get('.p-iconfield > .p-inputtext').type('MSD');
+            cy.get('#clientgroup_0').click();
+            cy.get('#Clientuser > .p-select-dropdown').click();
+            cy.get('#Clientuser_0').click();
+            cy.get('.grid > :nth-child(2) > .flex').click();
+            //creating a new Client Address
+            cy.get('#ClientAdditionalAddress > .p-select-label').click();
+            cy.get('.p-3 > :nth-child(1) > .flex').click();
 
-
+            
    });
 });       
