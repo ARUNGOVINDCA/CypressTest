ab4b833034bcdd5824dc567424d36160f56a99d4
arun <arun@ceroen.com>
2025-06-18 16:40:14 +0530

    Qt Update

--- cypress/e2e/Quotes.cy.js
+++ cypress/e2e/Quotes.cy.js
@@ -19,6 +19,14 @@ describe('Quote Creation', () => {
             //creating a new Client Address
             cy.get('#ClientAdditionalAddress > .p-select-label').click();
             cy.get('.p-3 > :nth-child(1) > .flex').click();
+            cy.get(':nth-child(2) > .p-floatlabel > #Price').type('2256687');
+            cy.get(':nth-child(3) > .p-floatlabel > #Price').type('9878866357');
+            cy.get(':nth-child(4) > .p-floatlabel > #Price').type('2256687');
+            cy.get(':nth-child(5) > .p-floatlabel > #Unit').type('haina1100@gmail.com')
+            cy.get(':nth-child(6) > .p-floatlabel > #Code').type('2nd street,Sevanth Empire Road');
+            cy.get(':nth-child(7) > .p-floatlabel > #Code').type('Karama City');
+            cy.get(':nth-child(8) > .p-floatlabel > #Price').type('');
+
 
             
    });
