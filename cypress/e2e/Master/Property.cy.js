253be58142d444a6e85e45d38fe6de7158615ee0
arun <arun@ceroen.com>
2025-07-03 10:40:39 +0530

    master brach created and property updt

--- cypress/e2e/Master/Property.cy.js
+++ cypress/e2e/Master/Property.cy.js
@@ -49,7 +49,59 @@ describe('Property Management Tests', () => {
       .should('be.visible')
       .click();
 
-    // Optionally, assert form state or click Save button here
-    // e.g. cy.get('.btn-save').click();
+    cy.get('#clientgroup > .p-select-dropdown')
+      .should('be.visible')
+      .click();
+    cy.wait(1000);
+    cy.get('#clientgroup_100')
+      .scrollIntoView()
+      .click();
+
+
+    //  Save button here
+    // cy.get('.gap-2 > .btn-new > .p-ripple').click();
   });
+  it.only('should Activate inactive Property', () => {
+    cy.get('#pn_id_1_tab_1 > .pi')
+      .scrollIntoView()
+      .should('be.visible')
+      .click();
+
+    cy.get('[routerlink="/property"]')
+      .scrollIntoView()
+      .should('be.visible')
+      .click();
+
+    cy.get(':nth-child(12) > .items-start > .w-5\\/6 > .text-sm')
+      .should('be.visible')
+      .click();
+
+    cy.get('.btn-new > .p-ripple')
+      .should('be.visible')
+      .click();
+
+    cy.get('.p-popover-content > :nth-child(2)')
+      .should('be.visible')
+      .click();
+
+    cy.get('.p-select-label')
+      .should('be.visible')
+      .first() // Fix: avoid multiple match error
+      .click();
+    cy.wait(1000);
+    cy.get('#pn_id_16_0')
+      .should('be.visible')
+      .click();
+
+    cy.get('.justify-between > .bg-green-500')
+      .first()
+      .click();
+
+
+    cy.get('[routerlink="/property"]')
+      .scrollIntoView()
+      .should('be.visible')
+      .click();
+  });
+
 });
